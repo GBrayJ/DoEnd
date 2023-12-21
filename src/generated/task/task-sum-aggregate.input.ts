f7948b24969ca313d354edd90d31e5c1a3b48b6e
Brayden Johnson <brayden@braydenjohnson.dev>
2023-12-20 20:54:20 -0700

    Delete src/generated directory
    Removed Directory -- Alike To Node Modules Directory. Generate on local, don't push remote.

--- src/generated/task/task-sum-aggregate.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-
-@InputType()
-export class TaskSumAggregateInput {
-
-    @Field(() => Boolean, {nullable:true})
-    priority?: true;
-}
