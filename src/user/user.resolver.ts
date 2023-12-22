0398b7e4dafeb872228c3fdedc9bf16d8bd0a473
Brayden Johnson <brayden@braydenjohnson.dev>
2023-12-21 21:14:38 -0700

    prettify is annoying

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,16 +1,26 @@
-import { Args, Mutation, Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from '../generated/user/user.model';
 import { FindUniqueUserArgs } from '../generated/user/find-unique-user.args';
 import { CreateOneUserArgs } from '../generated/user/create-one-user.args';
 import { DeleteOneUserArgs } from '../generated/user/delete-one-user.args';
 import { UpdateOneUserArgs } from '../generated/user/update-one-user.args';
-import { Task } from "../generated/task/task.model";
-import { TaskService } from "../task/task.service";
+import { Task } from '../generated/task/task.model';
+import { TaskService } from '../task/task.service';
 
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private userService: UserService, private taskService: TaskService) {}
+  constructor(
+    private userService: UserService,
+    private taskService: TaskService,
+  ) {}
 
   @Query(() => User)
   async user(@Args() findUniqueUserArgs: FindUniqueUserArgs): Promise<User> {
@@ -41,7 +51,8 @@ export class UserResolver {
 
   @ResolveField()
   async tasks(@Parent() user: User): Promise<Task[]> {
-    return this.taskService.findMany({ where: {
+    return this.taskService.findMany({
+      where: {
         userId: user.id,
       },
     });
